a0952205720b9738a54e9e9b99c76b0cdd8a525c
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-20 16:26:44 -0300

    Criado funçao assincrona

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 function catchError(error) {
-    throw new Error(chalk.red(error.code, '404 - Not Found'));
+    throw new Error(chalk.red(error));
 }
 
 function getFile(filePath) {
     const enconding = 'utf-8';
-    fs.readFile(
-        filePath,
-        enconding,
-        (error, texto) => { 
-            error ? catchError(error) : console.log(chalk.green(texto));
-        }
-    );
+    fs.promises
+        .readFile(filePath, enconding)
+        .then(data => {
+            console.log(chalk.green(data));
+        })
+        .catch(error => {
+        	catchError(error);
+        })
 }
+
+// function getFile(filePath) {
+//     const enconding = 'utf-8';
+//     fs.readFile(
+//         filePath,
+//         enconding,
+//         (error, texto) => { 
+//             error ? catchError(error) : console.log(chalk.green(texto));
+//         }
+//     );
+// }
 
 getFile('./arquivos/texto.md');
