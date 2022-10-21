c098f8eee32eca76a5b5d87acf237dd3fd4db038
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-21 09:59:32 -0300

    Refatorado funcao para ser async

--- index.js
+++ index.js
@@ -5,27 +5,17 @@ function catchError(error) {
     throw new Error(chalk.red(error));
 }
 
-function getFile(filePath) {
-    const enconding = 'utf-8';
-    fs.promises
-        .readFile(filePath, enconding)
-        .then(data => {
-            console.log(chalk.green(data));
-        })
-        .catch(error => {
-        	catchError(error);
-        })
-}
+async function getFile(filePath) {
+    try {
+        const enconding = 'utf-8';
+        const text = await fs.promises.readFile(filePath, enconding);
+        console.log(chalk.green(text));
+    } catch(error) {
+        catchError(error);
+    } finally {
+        console.log(chalk.yellow("Operação concluída!"));
+    }
 
-// function getFile(filePath) {
-//     const enconding = 'utf-8';
-//     fs.readFile(
-//         filePath,
-//         enconding,
-//         (error, texto) => { 
-//             error ? catchError(error) : console.log(chalk.green(texto));
-//         }
-//     );
-// }
+}
 
 getFile('./arquivos/texto.md');
