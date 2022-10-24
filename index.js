82f8175cc3158be03b6ebe140186436dec66013a
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-24 14:53:21 -0300

    Criado método para pegar links com regex

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ async function getFile(filePath) {
     try {
         const enconding = 'utf-8';
         const text = await fs.promises.readFile(filePath, enconding);
-        console.log(chalk.green(text));
+        console.log(getLinks(text));
+
     } catch(error) {
         catchError(error);
     } finally {
         console.log(chalk.yellow("Operação concluída!"));
     }
-
 }
+
+function getLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+    return resultados;
+}
+
+// /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
 
 getFile('./arquivos/texto.md');
